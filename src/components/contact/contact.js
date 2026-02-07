af05ba0baa8a6a4bc7f26b04845dbcedeecace67
ygev <1816215+ygev@users.noreply.github.com>
2026-02-06 19:11:55 -0800

    Update contact.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -8,8 +8,8 @@ export default props => (
     <>
         <section className="contact">
             <h3 className="contact__txt">
-                <h4 className="contact__head">Hello.</h4>
-                I'm working at an AI startup now and don't update this site. These are my only social media, and I don't check them:</h3>
+                <h4 className="contact__head">Hey!</h4>
+                The robots.txt on this one is gnarly, so good job finding this website somehow. My life online is very unlisted, but I can be found in these web establishments:</h3>
             <div className="btn__contact--wrapper">
                 <a target="_blank" rel="noopener noreferrer" href="https://are.na/yana"><button className="btn__contact">Are.Na</button></a>
                 <a target="_blank" rel="noopener noreferrer" href="https://github.com/ygev"><button className="btn__contact">GitHub</button></a>
