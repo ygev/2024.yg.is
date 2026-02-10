2247bfc14e82aaeb0d22c93d8accf33a98ffa4af
ygev <1816215+ygev@users.noreply.github.com>
2026-02-09 21:08:41 -0800

    Fix the flashing from innerHero and innerTxt by removing <Fade>

--- src/components/innerHero/innerHero.js
+++ src/components/innerHero/innerHero.js
@@ -2,16 +2,13 @@ import React from "react";
 import "./innerHero.css";
 import "../../css/normalize.css";
 import "../../css/global.css";
-import Fade from 'react-reveal/Fade';
 
 export default props => (
     <>
-        <Fade delay={100} duration={1000}>
-            <section className="hero innerHero">
-                <figure className="hero__img">
-                    <img alt="" className="inner__img--limiter" src={props.img} />
-                </figure>
-            </section>
-        </Fade>
+        <section className="hero innerHero">
+            <figure className="hero__img">
+                <img alt="" className="inner__img--limiter" src={props.img} />
+            </figure>
+        </section>
     </>
 );
